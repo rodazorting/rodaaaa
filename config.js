4702fa8000398bba6b0ed2a2e49ab8cbfb8ba945
rodazorting <128248189+rodazorting@users.noreply.github.com>
2023-05-15 16:44:03 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    discord:{
+        id: "223850348601671680", 
+    },
+    github:{
+        token1: "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",
+        token2: ""
+    },
+    about:{
+        description: "Full-Stack Developer.",
+    },
+    social:{
+        github: "https://github.com/rodazorting",
+        instagram: "https://instagram.com/hlplkhrmn",
+    },
+    meta:{
+        title: 'roda',
+        description: 'roda',
+        theme: '#870312',
+        lang: 'en',
+        url: 'https://rodadev.net',
+        image: 'https://cdn.discordapp.com/attachments/1076901714759782430/1107653328869003324/RODADEV2.png?size=1024',
+    },
+    title: 'roda',
+    titles:[
+        {title: 'Home', url: '/', icon: 'fa-house'},
+        // {title: 'Projects', url: '/projects' ,icon: 'fa-blog'},
+        {title: 'Blog', url: '/blog' , icon: 'fa-code'},
+    ]
+}
